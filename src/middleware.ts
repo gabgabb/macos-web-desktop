9dfe8cd0ff88edf4bb41096fe0238d76448546ad
gabgab <gabifiil68@gmail.com>
2026-01-19 23:47:11 +0100

    Add env

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,12 @@ import { NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
 
-    if (pathname.startsWith("/lock") || pathname.startsWith("/api")) {
+    if (
+        pathname.startsWith("/lock") ||
+        pathname.startsWith("/api") ||
+        pathname.startsWith("/_next") ||
+        pathname.startsWith("/favicon")
+    ) {
         return NextResponse.next();
     }
 
