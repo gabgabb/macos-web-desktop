9a79404ed0c86e5a55579a73b88beb2141d319af
gabgab <gabifiil68@gmail.com>
2026-01-21 13:58:18 +0100

    make CI

--- tests/lockscreen.spec.ts
+++ tests/lockscreen.spec.ts
@@ -11,3 +11,11 @@ test("unlock removes lockscreen", async ({ page }) => {
     await expect(page.getByTestId("lock-screen")).not.toBeVisible();
     await expect(page.getByTestId("desktop")).toBeVisible();
 });
+
+test("lockscreen after clicking on lock icon", async ({ page }) => {
+    await bootDesktop(page);
+
+    await page.getByTestId("lock-icon").click();
+
+    await expect(page.getByTestId("lock-screen")).toBeVisible();
+});
